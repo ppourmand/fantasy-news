1c906f08f6479d348ee3174ac17d45fd2defe1ca
Pasha Pourmand <pasha.pourmand@gmail.com>
2018-03-11 21:03:04 -0700

    you can now change sources! default is br

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-let config = require('./config.js')
+let config = require('./config.js');
+const shell = require('electron').shell;
 
 // installing jquery, bootstrap, and popper (for dropdown menu)
 global.jQuery = require('jquery');
-require('popper.js')
-require('bootstrap')
+require('popper.js');
+require('bootstrap');
 
 // call the news api w/ proper parameters, set the data on the screen
-function getNews(playerName) {
+function getNews(playerName, source) {
     var url = 'https://newsapi.org/v2/everything?' +
             'q=' + playerName + "&" +
             'from=2017-01-09&' +
             'to=2018-03-04&' +
             'sortBy=popularity&' +
-            'sources=bleacher-report&' + 
+            'sources='+ source + "&" + 
             'apiKey='+ config.secrets.MY_KEY;
 
     var req = new Request(url);
@@ -45,9 +46,9 @@ function getNews(playerName) {
             let newRow = document.createElement("div");
             newRow.setAttribute("class", "row");
 
-            //let player = document.createElement("div");
-            //player.setAttribute("class", "col-2 border border-primary");
-            //player.innerHTML = playerName;
+            // let player = document.createElement("div");
+            // player.setAttribute("class", "col-2 border border-primary");
+            // player.innerHTML = playerName;
 
             let articleDiv = document.createElement("div");
             articleDiv.setAttribute("class", "col-10 border border-primary");
@@ -59,7 +60,6 @@ function getNews(playerName) {
             article.appendChild(articleTitle);
             articleDiv.appendChild(article);
             
-            newRow.appendChild(player);
             newRow.appendChild(articleDiv);
             main.appendChild(newRow);
         });
@@ -73,9 +73,9 @@ document.getElementById("submit-button").addEventListener("click", function(){
     if(document.getElementById("main") !== null){
         document.body.removeChild(document.getElementById("main"));
     }
-    let playerText = document.getElementById("player-name-form").value;
-    addPlayerToList(player);
-    getNews(playerText);
+    let playerText = document.getElementById("player-name-form").value.replace(/ /g,"%20");;
+    let source = getUserSourceChoice();
+    getNews(playerText, source);
 });
 
 // open links in browser instead of app
@@ -87,12 +87,21 @@ document.addEventListener('click', function (event) {
   }
 });
 
-function addPlayerToList(playerName, row) {
-    let player = document.createElement("div");
-    player.setAttribute("class", "col-2 border border-primary");
-    player.innerHTML = playerName;
+function getUserSourceChoice() {
+   let choice = document.getElementById("dropdownMenuButton").textContent;
 
-    row.appendChild(player)
+   if(choice === "NFL.com ") {
+       return "nfl-news";
+   }
+   else if(choice === "ABC via ESPN ") {
+       return "espn";
+   }
+   else if(choice === "Fox Sports ") {
+       return "fox-sports";
+   }
+   else {
+        return "bleacher-report";
+   }
 }
 
 // Changes the dropdown menu text to whatever the user clicks
